fd538013b8a82d0c4d76b288e2673e5a4aadf7f9
myfang <myfang97@gmail.com>
2020-10-12 17:53:37 +0800

    测试

--- BaiDuAi.py
+++ BaiDuAi.py
@@ -1,9 +1,7 @@
 # 百度人脸识别请求接口
 import requests
 from utils import getImgB64
-
-BD_request_url = "https://aip.baidubce.com/rest/2.0/face/v3/detect"
-
+import logging
 
 def getBdAccessToken():
     """
@@ -11,33 +9,59 @@ def getBdAccessToken():
     client_id 为官网获取的AK， client_secret 为官网获取的SK
     :return: access_token
     """
-    host = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=qGadE2QPlgHQnQXawp6PG6DD&client_secret=SGOv81lUOnYKVPiqvXN0yx4evwDOTtpl"
+    host = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=DGNYtO6GyGOjNuGgggoi4ZPN&client_secret=vl28C4qIV3GbGwWMo9WNPNsB3Yc8SqKQ"
     response = requests.get(host)
     # if response:
     return response.json()["access_token"]
 
 
-def BDAI_Face(imageUrl, access_token):
+access_token = getBdAccessToken()
+
+
+def BDAI_Face(imageUrl):
     """
-    百度AI人脸识别接口
+    百度AI人脸属性识别接口
     :param imageUrl:图片路径
     :param access_token:百度access_token
     :return:
     """
+    Bd_Face_request_url = "https://aip.baidubce.com/rest/2.0/face/v3/detect"
     params = {
-        "image": getImgB64(imageUrl),
-        "image_type": "BASE64",
-        "face_field": "age,beauty,expression,face_shape,gender,glasses,"
-                      "landmark,landmark150,race,quality,eye_status,emotion,"
-                      "face_type,mask,spoofing",
+        "image":
+        getImgB64(imageUrl),
+        "image_type":
+        "BASE64",
+        "face_field":
+        "age,beauty,expression,face_shape,gender,glasses,"
+        "landmark,landmark150,race,quality,eye_status,emotion,"
+        "face_type,mask,spoofing",
     }
 
     # access_token = getBdAccessToken()
-    request_url = BD_request_url + "?access_token=" + access_token
+    request_url = Bd_Face_request_url + "?access_token=" + access_token
     headers = {"content-type": "application/json"}
     response = requests.post(request_url, data=params, headers=headers)
     if response:
         return response.json()
 
 
-access_token = getBdAccessToken()
+def BDAI_Poem(word):
+    """
+    百度AI根据关键词自动生成7言绝句
+    word：关键词
+    """
+    Bd_Poen_request_url = "https://aip.baidubce.com/rpc/2.0/creation/v1/poem"
+    headers = {'Content-Type':'application/json'}
+    index = 0
+    params = {
+        'text' : word.encode('utf-8'),
+        'index' : index
+    }
+    logging.info('params:{}'.format(params))
+    request_url = Bd_Poen_request_url + "?access_token=" + access_token
+    headers = {"content-type": "application/json"}
+    response = requests.post(request_url, data=params, headers=headers)
+    if response:
+        index += 1
+        return response.json()
+    
--- app.py
+++ app.py
@@ -4,29 +4,36 @@ from flask import Flask, request
 import requests
 import base64
 import json
-from BaiDuAi import BDAI_Face, access_token
+from BaiDuAi import BDAI_Face
+from BaiDuAi import BDAI_Poem
 import time
 import logging
+from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder="static")
 
 # static_image_url = '/home/ubuntu/HFAI/static/images'
 
 
-@app.route("/")
+@app.route("/index")
 def hello_world():
     # print('hi')
-    return "欢迎来到我们的世界"
+    return render_template('index.html')
 
 
-@app.route("/api/bdai", methods=['POST'])
-def baiDuAi():
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/api/bdai_face", methods=['POST'])
+def baiDuAiFace():
     image_path = request.form['image_path']
     logging.info('image_path:{}'.format(image_path))
     small_image_path = str(image_path).split('.')[0] + '_' + str(
         time.time())[:8] + '_small.' + str(image_path).split('.')[1]
     logging.info('small_image_path:{}'.format(small_image_path))
-    res = BDAI_Face(image_path, access_token)
+    res = BDAI_Face(image_path)
     # logging.info('res:{}'.format(res))
     result = {}
     result["code"] = res["error_code"]  # 状态码
@@ -48,32 +55,58 @@ def baiDuAi():
         # 根据人脸位置获取小图并返回
         getSmallImage(image_path, small_image_path, bbox)
         result['data']['small_image_path'] = small_image_path
-
-        # a = int(sz1 / 2 - 64)  # x start 高
-        # b = int(sz1 / 2 + 64)  # x end
-        # c = int(sz2 / 2 - 64)  # y start 宽
-        # d = int(sz2 / 2 + 64)  # y end
-        # cropImg = image[a:b, c:d]  #裁剪图像
-        # cv2.imwrite(dest, cropImg)
-
         result["data"]["age"] = res['result']["face_list"][0]["age"]  # 年龄
         result["data"]["beauty"] = res['result']["face_list"][0][
             "beauty"]  # 颜值
-        result["data"]["expression"] = res['result']["face_list"][0][
-            "expression"]["type"]  # 表情none:不笑；smile:微笑；laugh:大笑
-        result["data"]["face_shape"] = res['result']["face_list"][0][
-            "face_shape"][
-                "type"]  # 脸型square: 正方形 triangle:三角形 oval: 椭圆 heart: 心形 round: 圆形
-        result["data"]["gender"] = res['result']["face_list"][0]["gender"][
-            "type"]  # male:男性 female:女性
-        result["data"]["glasses"] = res['result']["face_list"][0]["glasses"][
-            "type"]  # none:无眼镜，common:普通眼镜，sun:墨镜
-        result["data"]["emotion"] = res['result']["face_list"][0]["emotion"][
-            "type"]  # 情绪angry:愤怒 disgust:厌恶 fear:恐惧 happy:高兴 sad: 伤心 surprise: 惊讶 neutral: 无情绪
-        result["data"]["race"] = res['result']["face_list"][0]["race"][
-            "type"]  # 肤色yellow: 黄种人 white: 白种人 black:黑种人 arabs: 阿拉伯人
+        brow = {'none': '不笑', 'smile': '微笑', 'laugh': '大笑'}
+        result["data"]["expression"] = brow[res['result']["face_list"][0][
+            "expression"]["type"]]  # 表情none:不笑；smile:微笑；laugh:大笑
+        feature = {
+            'square': '正方形',
+            'triangle': '三角形',
+            'oval': '椭圆',
+            'heart': '心形',
+            'round': '圆形'
+        }
+        result["data"]["face_shape"] = feature[
+            res['result']["face_list"][0]["face_shape"]
+            ["type"]]  # 脸型square: 正方形 triangle:三角形 oval: 椭圆 heart: 心形 round: 圆形
+        gender = {'male': '男性', 'female': '女性'}
+        result["data"]["gender"] = gender[res['result']["face_list"][0][
+            "gender"]["type"]]  # male:男性 female:女性
+        glasses = {'none': '无眼镜', 'common': '普通眼镜', 'sun': '墨镜'}
+        result["data"]["glasses"] = glasses[res['result']["face_list"][0][
+            "glasses"]["type"]]  # none:无眼镜，common:普通眼镜，sun:墨镜
+        emotion = {
+            'angry': '愤怒',
+            'disgust': '厌恶',
+            'fear': '恐惧',
+            'happy': '高兴',
+            'sad': '伤心',
+            'surprise': '惊讶',
+            'neutral': '无情绪'
+        }
+        result["data"]["emotion"] = emotion[
+            res['result']["face_list"][0]["emotion"]
+            ["type"]]  # 情绪angry:愤怒 disgust:厌恶 fear:恐惧 happy:高兴 sad: 伤心 surprise: 惊讶 neutral: 无情绪
+        race = {
+            'yellow': '黄种人',
+            'white': '白种人',
+            'black': '黑种人',
+            'arabs': '阿拉伯人'
+        }
+        result["data"]["race"] = race[res['result']["face_list"][0]["race"][
+            "type"]]  # 肤色yellow: 黄种人 white: 白种人 black:黑种人 arabs: 阿拉伯人
     logging.info('result:{}'.format(result))
     return json.dumps(result)
+
+
+@app.route("/api/bdai_poem", methods=['POST'])
+def baiDuAiPoem():
+    word = request.form['text']
+    logging.info('word:' + word)
+    res = BDAI_Poem(word)
+    return json.dumps(res)
 
 
 # 设置图片上传允许的文件格式
@@ -122,20 +155,19 @@ def upload():
         logging.info('filename:{}'.format(f.filename))
         # 处理上传图片位置
         filename = str(f.filename).split('.')[0] + '_' + str(
-            time.time())[:8] + str(f.filename).split('.')[1]
+            time.time())[:8] + '.' + str(f.filename).split('.')[1]
         basepath = os.path.dirname(__file__)  # 当前文件所在路径
         upload_path = os.path.join(basepath, 'static/images',
                                    filename)  # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
         f.save(upload_path)
+        show_path = os.path.join('static/images', filename)
         logging.info('upload_path:{}'.format(upload_path))
-        # 使用Opencv转换一下图片格式和名称
-        img = cv2.imread(upload_path)
-        cv2.imwrite(os.path.join(basepath, 'static/images', f.filename), img)
-
         result['code'] = 0
         result['msg'] = 'success'
         result['data'] = {}
-        result['data']['image_path'] = upload_path
+        result['data']['image_path'] = show_path
+        # result['data']['image_path'] = upload_path
+        # result['data']['show_path'] = show_path
         logging.info('result:{}'.format(result))
         return json.dumps(result)
 
@@ -144,7 +176,9 @@ if __name__ == "__main__":
     # logging.basicConfig(level=logging.INFO,
     #                     format='%(asctime)s %(filename)s(%(funcName)s)[line:%(lineno)d] %(levelname)s %(message)s',
     #                     datefmt='%a, %d %b %Y %H:%M:%S')
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s %(filename)s(%(funcName)s)[line:%(lineno)d] %(levelname)s %(message)s',
-                        datefmt='%a, %d %b %Y %H:%M:%S')
+    logging.basicConfig(
+        level=logging.INFO,
+        format=
+        '%(asctime)s %(filename)s(%(funcName)s)[line:%(lineno)d] %(levelname)s %(message)s',
+        datefmt='%a, %d %b %Y %H:%M:%S')
     app.run(host="0.0.0.0", port=9999, debug=True)
